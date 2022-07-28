d885d601a2ba447bf6458221fa3bc3cd2acd7981
EC2 Default User <ec2-user@ip-172-31-27-106.us-west-1.compute.internal>
2022-07-28 11:26:14 +0000

    ruby課題1修正

--- memo.rb
+++ memo.rb
@@ -5,7 +5,7 @@ memo_type = gets.to_s.chomp
 
 if memo_type == "1"
 puts "新規でメモを作成します。拡張子を除いたファイル名を入力してください。"
-file_name = gets
+file_name = gets.chomp
 puts"メモの内容を記入して下さい。Ctrl+Dで保存します。"
 memo = STDIN.read
 CSV.open("#{file_name}.csv" , "w") do |csv|
@@ -14,7 +14,7 @@ end
 
 elsif memo_type == "2"
 puts "既存のメモを編集します。拡張子を除いた既存ファイル名を入力してください。"
-file_name = gets
+file_name = gets.chomp
 CSV.open("#{file_name}.csv" , "a") do |csv|
 puts"メモの内容を編集して下さい。Ctrl+Dで保存します。"
 memo = STDIN.read
